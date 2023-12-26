0d693fb127f9364d8ef40a1c9b19e395f76ddacb
Michael Iwanek <miwanek11199@gmail.com>
2023-12-25 16:01:09 -0800

    have working version of app which uses an image url

--- apiRoutes.tsx
+++ apiRoutes.tsx
@@ -3,29 +3,39 @@ import axios from "axios";
 
 const router = express.Router();
 
-const fetchData = async () => {
+const fetchImageAnalysisUsingURL = async (imageUrl: string) => {
+  const apiKey = process.env.API_KEY_AWS;
+  const apiPytorchImageURL = process.env.AWS_GATEWAY_API_URL;
+
+  console.log("Function fetchImageAnalysisUsingURL is called. ")
+  //console.log("API Key:", apiKey);
+  //console.log("API Pytorch Image URL:", apiPytorchImageURL);
+
   try {
-    const response = await axios.post("/api/image-url-pytorch", {});
-    console.log("Response:", response?.data?.message);
-    alert(response?.data?.message);
+    const response = await axios.post(apiPytorchImageURL ?? "", {
+      image_url: imageUrl
+    });
+    console.log("Response:", response?.data);
+    return response?.data
   } catch (error: any) {
     console.error("Error:", error.message);
   }
 };
 
-router.post("/image-url-pytorch", (req, res) => {
+router.post("/image-url-pytorch", async (req, res) => {
   try {
-    const apiKey = process.env.API_KEY_AWS;
-    const sampleImageUrl =
-      "https://images.freeimages.com/images/large-previews/bd1/cat-1404368.jpg";
+    const { imageUrl } = req.body;
 
-    if (apiKey) {
-      console.log(`API Key: ${apiKey}`);
-    } else {
-      console.error("API Key not set.");
+    if (!imageUrl) {
+      return res
+        .status(400)
+        .json({ error: "Bad Request - Missing image_url in the request body." });
     }
 
-    return res.json({ message: "hello from the server 😎" });
+    const response = await fetchImageAnalysisUsingURL(imageUrl)
+
+
+    return res.json(response);
   } catch (err) {
     return res
       .status(400)
--- frontend/src/assets/components/HomePage.tsx
+++ frontend/src/assets/components/HomePage.tsx
@@ -1,11 +1,20 @@
 import axios from "axios";
+import { useState } from "react";
 
 const HomePage = () => {
-  const fetchData = async () => {
+  const [inputValue, setInputValue] = useState("");
+  const sampleImageUrl =
+    "https://images.freeimages.com/images/large-previews/bd1/cat-1404368.jpg";
+
+  const fetchPyTorchAnalysisUsingImageURL = async (imageUrl: string) => {
     try {
-      const response = await axios.post("/api/image-url-pytorch", {});
-      console.log("Response:", response?.data?.message);
-      alert(response?.data?.message);
+      const response = await axios.post("/api/image-url-pytorch", {
+        imageUrl: imageUrl,
+      });
+      // const data = response?.data
+      const jsonString = JSON.stringify(response?.data, null, 2); // the third argument (2) adds indentation for better readability
+      console.log("Response:", jsonString);
+      alert(jsonString);
     } catch (error: any) {
       console.error("Error:", error.message);
     }
@@ -15,12 +24,23 @@ const HomePage = () => {
     <>
       <div className="h-screen bg-slate-600 pt-8">
         <button
-          onClick={() => fetchData()}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => fetchPyTorchAnalysisUsingImageURL(sampleImageUrl)}
+          className="bg-red-800 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Use Sample Image URL
+        </button>
+        <button
+          onClick={() => fetchPyTorchAnalysisUsingImageURL(inputValue)}
+          className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-8 mr-8"
         >
           Submit Image URL
         </button>
-        <input className="ml-8 border-2 border-black"></input>
+        <input
+          type="text"
+          id="inputField"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
       </div>
     </>
   );
