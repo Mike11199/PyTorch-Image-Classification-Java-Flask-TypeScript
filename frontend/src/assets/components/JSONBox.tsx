50aa5679d062fd11fd4a60dfd3632f11aeb6c744
Michael Iwanek <miwanek11199@gmail.com>
2023-12-28 14:05:09 -0800

    fix bug json box

--- frontend/src/assets/components/JSONBox.tsx
+++ frontend/src/assets/components/JSONBox.tsx
@@ -11,20 +11,22 @@ const JSONBox = ({ loading, pyTorchImageResponseString }: JSONBoxProps) => {
   return (
     <>
       <div
-        className={`text-left pl-4 h-full ${!loading ? 'overflow-auto' : ''}`}
+        className={`text-left pl-4 h-full ${!loading ? "overflow-auto" : ""}`}
         style={{ backgroundColor: "#272822" }}
       >
-      {loading && (
+        {loading && (
           <div className="w-full flex justify-center">
             <LineWave height="100" width="100" color="green" />
           </div>
         )}
-        <JSONPretty
-          className="w-full h-full"
-          style={{ backgroundColor: "#272822" }}
-          id="json-pretty"
-          data={!loading && pyTorchImageResponseString}
-        ></JSONPretty>
+        {!loading && (
+          <JSONPretty
+            className="w-full h-full"
+            style={{ backgroundColor: "#272822" }}
+            id="json-pretty"
+            data={!loading && pyTorchImageResponseString}
+          ></JSONPretty>
+        )}
       </div>
     </>
   );
