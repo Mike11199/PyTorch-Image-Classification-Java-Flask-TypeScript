c2979f36f816d90782630418b50afc322e3d2128
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2025-02-12 20:19:17 -0800

    fix home page drop shadow desaturating video

--- frontend/src/assets/components/HomePage.tsx
+++ frontend/src/assets/components/HomePage.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <>
       <div className="flex-1 flex-col bg-[linear-gradient(#1c2a3f_0%,#223146_5%,#192433_95%,#1c2a3f_100%)] pt-4 pb-6">
         <div className="text-sm text-left md:mx-44 mt-2">
-          <div className="bg-black bg-opacity-60 p-8 md:rounded-lg">
+          <div className="bg-black bg-opacity-60 p-8 md:rounded-lg shadow-md shadow-black">
             <h1 className="font-bold text-orange-600 mb-2">Description</h1>
             <div className="ml-2 md:ml-8 text-gray-200">
               <li>
@@ -16,7 +16,7 @@ const HomePage = () => {
           </div>
           <div className="w-full flex justify-center mt-12">
             <video
-              className="md:rounded-lg drop-shadow-2xl shadow-black"
+              className="md:rounded-lg shadow-md shadow-black"
               width="640"
               height="360"
               autoPlay
